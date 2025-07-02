1927749877dfe9f387a6adc96962cca78582c987
Nadir ansari <nadiransari@Nadirs-MacBook-Air.local>
2025-07-03 01:48:58 +0530

    Updated config, ensured .env is not pushed

--- Downloads/userAuth/src/main/java/com/nadir/userAuth/UserAuthApplication.java
+++ Downloads/userAuth/src/main/java/com/nadir/userAuth/UserAuthApplication.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
+//import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class UserAuthApplication implements CommandLineRunner {
